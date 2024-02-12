7cb01b05073987fd71bb5a12204e162c6dff9da3
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-02-12 11:07:18 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,166 +1,65 @@
 // Note: SOURCE = output, TARGET = input. Yes; this is confusing
 
-import { useCallback, useState } from 'react';
+import {useCallback, useState} from 'react';
 import ReactFlow, {
-  Controls,
-  Background,
-  MiniMap,
-  useNodesState,
-  useEdgesState,
-  NodeChange,
-  EdgeChange,
+  addEdge,
   applyEdgeChanges,
   applyNodeChanges,
-  addEdge,
+  Background,
+  BackgroundVariant,
   Connection,
+  Controls,
+  Edge,
+  EdgeChange,
+  MiniMap,
+  Node,
+  NodeChange,
   NodeResizer,
   NodeToolbar,
+  NodeTypes
 } from 'reactflow';
-import { TextNode } from './custom_nodes/text';
-
-import { NodeTemplate } from './custom_nodes2/node';
-import { videoModelDef } from './custom_nodes2/nodeVideoModel';
-import { NodeData } from './custom_nodes2/types';
-import { imageUpscaleWithModelDef, upscaleModelLoaderDef } from './custom_nodes2/nodeUpscaleModel';
 
 import 'reactflow/dist/style.css';
-import './custom_nodes/text.css';
 
-type NodeType = {
-  id: string;
-  type?: string;
-  data: { text?: string; label?: string; };
-  position: { x: number; y: number; };
-};
-
-const initialNodeTypes = { 
-  ClipTextEncode: TextNode, 
-  videoNode: () => <NodeTemplate data={videoModelDef} />,
-  upscaleModelLoader: () => <NodeTemplate data={upscaleModelLoaderDef} />,
-  upscaleWithModel: () => <NodeTemplate data={imageUpscaleWithModelDef} />,
-};
-
-const edgeTypes = { CLIP: 'clip' };
-
-const initNodes: NodeType[] = [
-    {
-    id: 'VideoA',
-      type: 'videoNode',
-    data: { text: 'Node A' },
-    position: { x: 250, y: 0 },
-  },
-  {
-    id: 'TextA',
-      type: 'ClipTextEncode',
-    data: { text: 'Node A' },
-    position: { x: 250, y: 0 },
-  },
-  {
-    id: 'TextB',
-    type: 'ClipTextEncode',
-    data: { text: 'Node B' },
-    position: { x: 100, y: 100 },
-  },
-  {
-    id: 'PositivePrompt',
-    type: 'ClipTextEncode',
-    data: { text: 'Hello world' },
-    position: { x: 120, y: 130 },
-  },
-    {
-    id: 'Input',
-    type: 'input',
-    data: { label: 'input' },
-    position: { x: 120, y: 130 },
-  },
-      {
-    id: 'Output',
-    type: 'output',
-    data: { label: 'output' },
-    position: { x: 120, y: 130 },
-  },
-  {
-    id: 'Group',
-    type: 'group',
-    data: { label: 'group' },
-    position: { x: 0, y: 0 },
-  },
-    {
-    id: 'UpscaleLoader',
-    type: 'upscaleModelLoader',
-    data: { label: 'group' },
-    position: { x: 0, y: 0 },
-  },
-    {
-    id: 'UpscaleWithModel',
-    type: 'upscaleWithModel',
-    data: { label: 'group' },
-    position: { x: 0, y: 0 },
-  },
-];
-
-
-const initEdges = [
-  {
-    id: 'A->B',
-    source: 'TextA',
-    target: 'TextB',
-  },
-];
 
 function App() {
-  // const [nodes, setNodes, onNodesChange] = useNodesState(initNodes);
-  // const [edges, setEdges, onEdgesChange] = useEdgesState(initEdges);
-  const [nodes, setNodes] = useState(initNodes);
-  const [edges, setEdges] = useState(initEdges);
-  const [nodeTypes, setNodeTypes] = useState(initialNodeTypes);
+    const [nodes, setNodes] = useState<Node[]>([]);
+    const [edges, setEdges] = useState<Edge[]>([]);
+    const [nodeTypes, setNodeTypes] = useState<NodeTypes>({});
 
-  // Standard React Flow Handlers
-  const onNodesChange = useCallback(
-    (changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds as NodeType[])),
-    [setNodes]
-  );
-  const onEdgesChange = useCallback(
-    (changes: EdgeChange[]) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
-  const onConnect = useCallback(
-    (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges]
-  );
+    // Standard React Flow Handlers
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes]
+    );
 
-  // Successor to ComfyUI function
-  function registerNodesFromDefs(defs: Record<string, NodeData>) {
-    for (const [key, value] of Object.entries(defs)) {
-        registerNode(key, value);
-    }
-  }
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges]
+    );
 
-  // We should let pluings register arbitrary components
-  function registerNode(nodeId: string, nodeData: NodeData) {
-    setNodeTypes((prev) => ({
-      ...prev,
-      [nodeId]: () => <NodeTemplate data={nodeData} />
-    }));
-  }
+    const onConnect = useCallback(
+        (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges]
+    );
 
-  return (
-    <ReactFlow
-      nodes={nodes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      nodeTypes={nodeTypes}
-      fitView
-    >
-      <Background />
-      <Controls />
-      <MiniMap />
-      <NodeResizer />
-      <NodeToolbar />
-    </ReactFlow>
-  );
+    return (
+        <ReactFlow
+            nodes={nodes}
+            onNodesChange={onNodesChange}
+            edges={edges}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            nodeTypes={nodeTypes}
+            fitView
+        >
+            <Background variant={BackgroundVariant.Lines}/>
+            <Controls/>
+            <MiniMap/>
+            <NodeResizer/>
+            <NodeToolbar/>
+        </ReactFlow>
+    );
 }
 
 export default App;
