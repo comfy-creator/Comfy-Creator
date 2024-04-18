78669788a72536822a7d0572789bc11720aad5ba
Paul Fidika <paul@fidika.com>
2024-04-17 19:23:16 -0600

    configure vite-js to build library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import * as fs from 'node:fs';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { glob } from 'glob';
 
 const { dependencies, peerDependencies } = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
 
@@ -9,29 +13,45 @@ const { dependencies, peerDependencies } = JSON.parse(fs.readFileSync(`./package
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       rollupTypes: true,
-      afterBuild: () => {
-        fs.copyFileSync('dist/index.d.ts', 'dist/index.d.cts');
-      }
+      include: ['lib']
+      // afterBuild: () => {
+      //   fs.copyFileSync('dist/index.d.ts', 'dist/index.d.cts');
+      // }
     })
   ],
   build: {
     target: 'es2017',
     lib: {
-      fileName: 'index',
-      formats: ['cjs', 'es'],
-      entry: 'src/App.tsx'
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
     },
     emptyOutDir: true,
-    sourcemap: true,
+    // sourcemap: true,
     minify: true,
     rollupOptions: {
       external: [
-        ...Object.keys(dependencies || {}),
+        // ...Object.keys(dependencies || {}),
         ...Object.keys(peerDependencies || {}),
         'react/jsx-runtime'
-      ]
+      ],
+      input: Object.fromEntries(
+        glob.sync('lib/**/*.{ts,tsx}').map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative('lib', file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        // Filenames outputted to dist
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
+      }
     }
   }
 });
