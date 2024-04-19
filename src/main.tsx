a6c61bb7241dd302ec1b20cdb8cc73ac02e7362f
Paul Fidika <paul@fidika.com>
2024-04-19 00:21:59 -0600

    try new minified bundled build for the library

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import GraphEditor from '../lib/main';
+import GraphEditor from '../lib';
 
 import './index.css';
 
--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,14 @@ const { peerDependencies } = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'es2017',
+    target: 'ESNext',
     lib: {
-      entry: resolve(__dirname, 'lib/main.tsx'),
-      formats: ['es']
+      entry: resolve(__dirname, 'lib/index.tsx'),
+      name: 'GraphEditor',
+      formats: ['es', 'umd']
     },
     emptyOutDir: true,
-    // sourcemap: true,
+    sourcemap: true,
     minify: true,
     rollupOptions: {
       external: [
@@ -27,19 +28,19 @@ export default defineConfig({
         ...Object.keys(peerDependencies || {}),
         'react/jsx-runtime'
       ],
-      input: {
-        ...Object.fromEntries(
-          glob.sync('lib/**/*.{ts,tsx}').map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative('lib', file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url))
-          ])
-        ),
-        main: resolve(__dirname, 'index.html')
-      },
+      // input: {
+      //   ...Object.fromEntries(
+      //     glob.sync('lib/**/*.{ts,tsx}').map((file) => [
+      //       // The name of the entry point
+      //       // lib/nested/foo.ts becomes nested/foo
+      //       relative('lib', file.slice(0, file.length - extname(file).length)),
+      //       // The absolute path to the entry file
+      //       // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+      //       fileURLToPath(new URL(file, import.meta.url))
+      //     ])
+      //   ),
+      //   main: resolve(__dirname, 'index.html')
+      // },
       output: {
         // Filenames outputted to dist
         assetFileNames: 'assets/[name][extname]',
@@ -53,9 +54,6 @@ export default defineConfig({
     dts({
       include: ['lib/**/*.tsx', 'lib/**/*.ts'],
       rollupTypes: true
-      // afterBuild: () => {
-      //   fs.copyFileSync('dist/main.d.ts', 'dist/main.d.cts');
-      // }
     })
   ]
 });
