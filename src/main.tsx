f15b4240abb9568abb21b9a55322350febcceda7
Paul Fidika <paul@fidika.com>
2024-04-19 18:03:30 -0600

    simplify vite.config file, get umd.cjs files to be generated as well as es modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import GraphEditor from '../lib';
 
 import './index.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GraphEditor />
-  </React.StrictMode>,
-  document.getElementById('root')!
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,41 @@
-import * as fs from 'node:fs';
 import { resolve } from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
-const { peerDependencies } = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
+// const { peerDependencies } = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    build: {
-      target: 'ESNext',
-      lib: {
-        entry: resolve(__dirname, 'lib/index.tsx'),
-        name: 'GraphEditor',
-        formats: ['es', 'umd']
-      },
-      emptyOutDir: true,
-      sourcemap: true,
-      minify: true,
-      rollupOptions: {
-        external: [
-          // ...Object.keys(dependencies || {}),
-          ...Object.keys(peerDependencies || {}),
-          'react/jsx-runtime'
-        ],
-        // input: {
-        //   ...Object.fromEntries(
-        //     glob.sync('lib/**/*.{ts,tsx}').map((file) => [
-        //       // The name of the entry point
-        //       // lib/nested/foo.ts becomes nested/foo
-        //       relative('lib', file.slice(0, file.length - extname(file).length)),
-        //       // The absolute path to the entry file
-        //       // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-        //       fileURLToPath(new URL(file, import.meta.url))
-        //     ])
-        //   ),
-        // main: resolve(__dirname, 'index.html')
-        // },
-        output: {
-          // Filenames outputted to dist
-          assetFileNames: 'assets/[name][extname]',
-          entryFileNames: '[name].js'
+export default defineConfig({
+  build: {
+    target: 'ESNext',
+    lib: {
+      entry: resolve(__dirname, 'lib/index.tsx'),
+      name: 'GraphEditor',
+      fileName: 'index',
+      formats: ['es', 'umd']
+    },
+    emptyOutDir: true,
+    sourcemap: true,
+    // minify: true,
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime'
         }
       }
-    },
-    define: {
-      'process.env': env
-    },
-    plugins: [
-      react(),
-      libInjectCss(),
-      dts({
-        include: ['lib/**/*.tsx', 'lib/**/*.ts'],
-        rollupTypes: true
-      })
-    ]
-  };
+    }
+  },
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ['lib/**/*.tsx', 'lib/**/*.ts'],
+      rollupTypes: true
+    })
+  ]
 });
