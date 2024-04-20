c4f9c8d223667836d4703f0a567dacd69e90cae7
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2024-04-20 18:49:32 +0100

    remove unused packages and config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
-import libCss from 'vite-plugin-libcss';
 
 // const { peerDependencies } = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
 
@@ -29,16 +28,11 @@ export default defineConfig({
           'react-dom': 'ReactDOM',
           'react/jsx-runtime': 'jsxRuntime'
         }
-      },
-      plugins: [
-        // postcss({ extract: 'styles.css' })
-        // add other Rollup plugins here if needed
-      ]
+      }
     }
   },
   plugins: [
     react(),
-    libCss(),
     libInjectCss(),
     dts({
       include: ['lib/**/*.tsx', 'lib/**/*.ts'],
