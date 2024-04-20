9696aece3730280a4604b9e250dfef166bba46ff
Paul Fidika <paul@fidika.com>
2024-04-20 15:50:32 -0600

    remove umd .cjs files, get rid of bundle mode and instead split files when they are built into /dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
-import { resolve } from 'path';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
@@ -14,15 +16,27 @@ export default defineConfig({
       entry: resolve(__dirname, 'lib/index.tsx'),
       name: 'GraphEditor',
       fileName: 'index',
-      formats: ['es', 'umd']
+      formats: ['es']
     },
     emptyOutDir: true,
-    sourcemap: true,
+    // sourcemap: true,
     // minify: true,
     cssCodeSplit: false,
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('lib/**/*.{ts,tsx}').map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative('lib', file.slice(0, file.length - extname(file).length)),
+          // The absolute path to the entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
       output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
@@ -35,7 +49,7 @@ export default defineConfig({
     react(),
     libInjectCss(),
     dts({
-      include: ['lib/**/*.tsx', 'lib/**/*.ts'],
+      include: ['lib'],
       rollupTypes: true
     })
   ]
